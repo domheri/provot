bbe8edd7419dacb414a712193c428abc9c7f4871
domheri <47629857+domheri@users.noreply.github.com>
2019-03-04 09:16:18 -0800

    Update probot.js

--- probot.js
+++ probot.js
@@ -416,7 +416,7 @@ client.on('message', message => {
   let embed = new Discord.RichEmbed()
 .setAuthor(message.author.username)
 .setColor("#9B59B6")
-.addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/NgdvJy3**")
+.addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/67m92V**")
   
   
 message.channel.sendEmbed(embed);
